2e29d43c0908d6cdea9d8a4e88d51d2650c19e3f
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-08-27 12:50:36 -0400

    added listgroup component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Message from "./Message";
 
 function App() {
   const items = ["New York", "San Francisco", "Delhi", "Hyderabad"];
-  return <div><ListGroup items={items}  heading ="Cities"></ListGroup></div>
+  const handleSelectedItem = (item:string)=>{
+    console.log(item)
+  }
+  return <div><ListGroup items={items}  heading ="Cities" onSelectItem={handleSelectedItem}></ListGroup></div>
 }
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,9 +4,10 @@ import { Fragment } from "react/jsx-runtime";
 interface Props{
   items: string[];
   heading : string;
+  onSelectItem:(item:string)=>void;
 }
 
-function ListGroup({items, heading}:Props) {
+function ListGroup({items, heading,onSelectItem}:Props) {
 
 const [selectedIndex, setSelectedIndex]=useState(-1);
 
@@ -23,7 +24,9 @@ const [selectedIndex, setSelectedIndex]=useState(-1);
           <li
             className= {selectedIndex===index?"list-group-item active": "list-group-item"}
             key={item}
-            onClick={()=>setSelectedIndex(index)}
+            onClick={()=>{setSelectedIndex(index) ;
+              onSelectItem(item);}}
+            
           >
             {item}
           </li>
