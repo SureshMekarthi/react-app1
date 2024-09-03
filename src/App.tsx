629c95b5ead25c939094ca9251a841dd2205d737
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-09-03 19:42:15 -0400

    added new alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 
 function App() {
-  const items = ["New York", "San Francisco", "Delhi", "Hyderabad"];
-  const handleSelectedItem = (item:string)=>{
-    console.log(item)
-  }
-  return <div><ListGroup items={items}  heading ="Cities" onSelectItem={handleSelectedItem}></ListGroup></div>
+  return<div><Alert>Hello <span>world</span></Alert></div>
 }
 export default App;
