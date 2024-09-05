54f9e95c84ccf7207f2ebb022856c80202bc92a6
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-09-04 23:22:44 -0400

    color is set to spefic values

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 interface Props{
     children: string;
-    color :string;
+    color?:'primary'|'secondary'|'danger';
     onClick:()=>void;
 }
 
-function Button({children, color, onClick}:Props) {
+function Button({children, color="primary", onClick}:Props) {
   return (
     <>
     <button type="button" className={"btn btn-"+ color} onClick={onClick}>{children}</button>
