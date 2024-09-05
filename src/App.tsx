e14ff66c3bb3c6789401e662cf6d0cfda5c0e413
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-09-05 19:32:14 -0400

    added alert when clicking button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 
 function App() {
-  return<div><Button onClick={()=>console.log("clicked")}>my button</Button></div>
+  const [alertvisibility, setalertVisibility] = useState(false);
+  return (
+    <div>
+      <Alert>Alert</Alert>
+      <Button onClick={() => setalertVisibility(true)}>my button</Button>
+    </div>
+  );
 }
 export default App;
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,12 +1,23 @@
 import { ReactNode } from "react";
 
-interface Props{
-    children:ReactNode;
+interface Props {
+  children: ReactNode;
 }
-const Alert = ({children}:Props) => {
+const Alert = ({ children }: Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
-  )
-}
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      {children} Hello!
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
 
-export default Alert
+export default Alert;
