451aae10bb9867f5b9b861f8e2c7a98b4c1128d8
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-09-04 22:47:27 -0400

    added button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 
 function App() {
-  return<div><Alert>Hello <span>world</span></Alert></div>
+  return<div><Button></Button></div>
 }
 export default App;
