b48e8fbe8bb646bcbb670090438cd85df95451c6
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-09-12 19:51:01 -0400

    added new drink object

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Message from "./components/Message";
 
 function App() {
+  const [drink, setDrink] = useState({
+    tittle: "Americano",
+    price: 5,
+  });
+  const handleClick = () => {
+    drink.price = 6;
+  };
+
   return (
     <div>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
+      <button onClick={handleClick}> Click me</button>
     </div>
   );
 }
