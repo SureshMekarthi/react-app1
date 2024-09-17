0d74636864cb8d1544e27558f8a4d7839fc59bab
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-09-16 23:03:04 -0400

    added navbar and cart components

---
+++ src/components/Cart.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+interface Props {
+  cartItems: string[];
+  onClick: () => void;
+}
+function Cart({ cartItems, onClick }: Props) {
+  return (
+    <>
+      <div>Cart</div>;
+      <ul>
+        {cartItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <button onClick={onClick}></button>
+    </>
+  );
+}
+
+export default Cart;
