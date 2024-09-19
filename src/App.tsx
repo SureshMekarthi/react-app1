364d13d6fcedf5bfb1c5594cd3f784f1c139ea10
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-09-19 19:14:41 -0400

    Added expandable text

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
 import produce, { Immer } from "immer";
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "product 2", quantity: 1 },
-    ],
-  });
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 2 } : item
-      ),
-    });
-
-    // setCart(
-    //   produce((draft) => {
-    //     const result = draft.items.find((item) => item.id === 1);
-    //     if (result) result.quantity = 2;
-    //   })
-    // );
-  };
   return (
-    <>
-      <div>
-        <button onClick={handleClick}>Click Me</button>
-      </div>
-      <ul></ul>
-    </>
+    <div>
+      <ExpandableText></ExpandableText>
+    </div>
   );
 }
 export default App;
---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExpandableText() {
+  return <div>ExpandableText</div>;
+}
+
+export default ExpandableText;
