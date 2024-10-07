5ffaea0cbb8554d5e0d0c6cf8d9637959d445d77
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-10-07 11:30:29 -0400

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import produce, { Immer } from "immer";
 import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi in
-        ea nulla. Possimus vitae ullam dolore odit deleniti optio beatae porro
-        id vero, quisquam natus at vel similique est suscipit!
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
