26e08dc3ef8d1875086fab12fb669c3fd3022588
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-11-01 13:00:40 -0400

    added visible expenses

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import produce, { Immer } from "immer";
 import { useState } from "react";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilitiy" },
     { id: 2, description: "bbb", amount: 15, category: "Utiliti" },
+    { id: 2, description: "ccc", amount: 25, category: "Utiliti" },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
       <ExpenseList
         expenses={expenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
