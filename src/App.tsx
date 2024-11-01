fd400f9b393f81a6cab30d64f9e0e564ec0f85ee
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-11-01 12:37:00 -0400

    added expense filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import produce, { Immer } from "immer";
 import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilitiy" },
+    { id: 2, description: "bbb", amount: 15, category: "Utiliti" },
+  ]);
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
     </div>
   );
 }
